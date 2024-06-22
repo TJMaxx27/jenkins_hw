b0d6ccfb1ad91269953d6aabbc2efa44210d6aaf
Your Name <you@example.com>
2024-06-23 00:02:34 +0400

    init

--- conftest.py
+++ conftest.py
@@ -3,15 +3,16 @@ from functools import wraps
 import allure
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.options import Options as FireFoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 logging.basicConfig(
     level=logging.ERROR, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
-
 
 def allure_attach_screenshot_on_failed(func):
     @wraps(func)
@@ -23,7 +24,6 @@ def allure_attach_screenshot_on_failed(func):
             raise e
     return wrapper
 
-
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--executor", action="store", default="172.17.0.2")
@@ -31,7 +31,6 @@ def pytest_addoption(parser):
     parser.addoption("--logs", action="store_true")
     parser.addoption("--video", action="store_true")
     parser.addoption("--bv", action="store", default="latest")
-
 
 @pytest.fixture()
 def browser(request):
@@ -42,28 +41,21 @@ def browser(request):
     logs = request.config.getoption("--logs")
     video = request.config.getoption("--video")
 
+    executor_url = f'http://{executor}:4444/wd/hub'
     options = None
-    driver = None
 
     if browser == "chrome":
         options = ChromeOptions()
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        chromedriver_path = "/usr/bin/chromedriver"
-        service = ChromeService(executable_path=chromedriver_path)
+        service = ChromeService(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
     elif browser == "firefox":
         options = FireFoxOptions()
-        options.binary_location = "/usr/bin/firefox"
-        geckodriver_path = "/usr/bin/geckodriver"
-        service = FirefoxService(executable_path=geckodriver_path)
+        service = FirefoxService(GeckoDriverManager().install())
         driver = webdriver.Firefox(service=service, options=options)
 
     capabilities = {
         'browserName': browser,
-        'browserVersion': version,
+        'browserVersion': version if version != "latest" else "",
         "selenoid:options": {
             "enableVNC": vnc,
             "name": request.node.name,
@@ -73,11 +65,16 @@ def browser(request):
         }
     }
 
-    if options:
-        for k, v in capabilities.items():
+    for k, v in capabilities.items():
+        if v:
             options.set_capability(k, v)
 
+    driver = None
     try:
+        driver = webdriver.Remote(
+            command_executor=executor_url,
+            options=options
+        )
         driver.maximize_window()
         yield driver
     finally:
